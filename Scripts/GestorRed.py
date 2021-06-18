9278f12e419e0c5c7f521fc2a8cfa720a8fb5ad6
Ray Pulido <ray_pulido@outlook.com>
2021-06-17 21:50:32 -0500

    Proyecto Redes3 SNMP

--- Scripts/GestorRed.py
+++ Scripts/GestorRed.py
@@ -3,7 +3,7 @@
 import BackupsFTP
 import SNMP
 import Alertas
-import os
+import os, re
 
 __author__ = "Cortés Castillo Daniela y Pulido Bejarano Raymundo"
 __copyright__ = "Copyright 2021, The R3 Project"
@@ -36,17 +36,17 @@ def Menu():
     print("Opciones:                                                  ")
     print("     1) Realizar Backup de todos los routers               ")    #Ready
     print("     2) Realizar Backup de un router                       ")    #Ready
-    print("     3) Restaurar un router con backup                     ")    #Ready
+    print("     3) Restaurar un router                                ")    #Ready
     print("     4) Listar Routers de la Red                           ")    #Ready
     print("     5) IP Host                                            ")    #Ready
     print("     6) Listar servidores y servicios                      ")    #Ready
     print("     7) Listar versiones de Backups                        ")    #Ready 
     print("     8) Borrar Backup                                      ")    #Ready 
-    print("     9) Listar Ip y mascara de interfaces de un router     ") #listar interfaces de un router( interfaz ip mask activo)
-    print("    10) Comprobar que las interfaces esten activas         ")
-    print("    11) Comprobar conectividad de Routers y servidores     ")
-    print("    12) Cambiar destino de alertas                         ") #Agregar identificar Routers y obtener ip de routers, Borrar Backups
-    print("    13) Salir                                              ")
+    print("     9) Listar Ip y mascara de interfaces de un router     ")    #Ready
+    print("    10) Comprobar que las interfaces esten activas         ")    #Ready
+    print("    11) Comprobar conectividad de Routers y servidores     ")    #Ready
+    print("    12) Cambiar destino de alertas                         ")    #Ready
+    print("    13) Salir                                              ")    #Ready
     print("-----------------------------------------------------------------------------------------------------------------")
     
 def Opcion1():
@@ -70,7 +70,7 @@ def Opcion2():
     if routers == None:
         routers = BackupsFTP.Obtener_ID_Router(ips_routers)
     
-    print("Los Router de la topologia son: \n")
+    print("Los Router de la topologia son: ")
     for router in routers.keys():
         print(f"    Router: {router}, IP:{routers[router]}")
 
@@ -92,7 +92,7 @@ def Opcion3():
     if routers == None:
         routers = BackupsFTP.Obtener_ID_Router(ips_routers)
 
-    print("Los Router de la topologia son: \n")
+    print("Los Router de la topologia son: ")
     for router in routers.keys():
         print(f"    Router: {router}, IP:{routers[router]}")
 
@@ -123,7 +123,7 @@ def Opcion4():
         ips_routers = BackupsFTP.obtener_ips_routers()
     if routers == None:
         routers = BackupsFTP.Obtener_ID_Router(ips_routers)
-    print("Los Router de la topologia son: \n")
+    print("Los Router de la topologia son: ")
     for router in routers.keys():
         print(f"    Router: {router}, IP:{routers[router]}")
 
@@ -137,7 +137,7 @@ def Opcion6():
         print(f"{servidor}:")
         print(f"    Servicios:")
         for service in Servidores[servidor][0]:
-            print(f"                {service}")
+            print(f"            {service}")
         print(f"    Ip:  {Servidores[servidor][1] }")
 
 def Opcion7():
@@ -165,7 +165,7 @@ def Opcion8():
     if routers == None:
         routers = BackupsFTP.Obtener_ID_Router(ips_routers)
 
-    print("Los Router de la topologia son: \n")
+    print("Los Router de la topologia son: ")
     for router in routers.keys():
         print(f"    Router: {router}, IP:{routers[router]}")
 
@@ -185,7 +185,7 @@ def Opcion8():
             
             res = BackupsFTP.BorrarBackup(backs_F[NameBack])
             if res:
-                print("Backup: {backs_F[NameBack]}  eliminado !!")
+                print(f"Backup: {backs_F[NameBack]}  eliminado !!")
             else:
                 print("Error al eliminar el backup")
         else:
@@ -194,8 +194,7 @@ def Opcion8():
         print(f"No existe el Router designado")
 
 def Opcion9():
-    global SNMPRouters, SNMPServidores
-    global ips_routers, routers
+    global SNMPRouters, ips_routers, routers
     
     if ips_routers == None:
         ips_routers = BackupsFTP.obtener_ips_routers()
@@ -205,14 +204,124 @@ def Opcion9():
         SNMPRouters = list()
         for router in routers.keys():
             SNMPRouters.append( SNMP.Snmp(nombreDevice=router , userName="R3SNMP",destHost=routers[router],protAuth="SHA",passAuth="RaDa22962") )
-    # infoR1_ifDesc = Router_4.walkSNMP("ifDescr")
-    # infoR1_ifAdminStatus = Router_4.walkSNMP("ifAdminStatus")
-    # infoR1_ipAdEntNetMask = Router_4.walkSNMP("ipAdEntNetMask")
-    # infoR1_ifName = Router_4.walkSNMP("ifName")
-    # infoR1_ipAdEntIfIndex = Router_4.walkSNMP("ipAdEntIfIndex")
+
+
+    print("Los Router de la topologia son: \n")
+    for router in routers.keys():
+        print(f"    Router: {router}, IP:{routers[router]}")
+
+    IDRouter = input("Ingrese el identificador del Router que desea consultar: ")    
+    if routers.get(IDRouter) :
+        for SNMPRouter in SNMPRouters:
+            if SNMPRouter.NombreDevice == IDRouter:
+                print(f"Obteniendo informacion del Router {IDRouter}")
+                res = SNMP.InfoIntRouter(SNMPRouter)
+                if res :
+                    print("Se obtuvo la informacion del router correctamente")
+                elif res == False:
+                    print(f"Error al obtener informacion del router {IDRouter}")
+                break                
+    else:
+        print(f"No existe el Router designado")
+
+def Opcion10():
+    global SNMPRouters, ips_routers, routers
+    
+    if ips_routers == None:
+        ips_routers = BackupsFTP.obtener_ips_routers()
+    if routers == None:
+        routers = BackupsFTP.Obtener_ID_Router(ips_routers)
+    if SNMPRouters == None:
+        SNMPRouters = list()
+        for router in routers.keys():
+            SNMPRouters.append( SNMP.Snmp(nombreDevice=router , userName="R3SNMP",destHost=routers[router],protAuth="SHA",passAuth="RaDa22962") )
+
+
+    print("Los Router de la topologia son: \n")
+    for router in routers.keys():
+        print(f"    Router: {router}, IP:{routers[router]}")
+
+    IDRouter = input("Ingrese el identificador del Router que desea consultar: ")    
+    if routers.get(IDRouter) :
+        for SNMPRouter in SNMPRouters:
+            if SNMPRouter.NombreDevice == IDRouter:
+                print(f"Obteniendo informacion del Router {IDRouter}")
+                res = SNMP.InfoActividadRouter(SNMPRouter)
+                if res :
+                    print("Se obtuvo la informacion del router correctamente")
+                elif res == False:
+                    print(f"Error al obtener informacion del router {IDRouter}")
+                break                
+    else:
+        print(f"No existe el Router designado")
+
+def Opcion11():
+    global SNMPRouters, ips_routers, routers, SNMPServidores
+    
+    if ips_routers == None:
+        ips_routers = BackupsFTP.obtener_ips_routers()
+    if routers == None:
+        routers = BackupsFTP.Obtener_ID_Router(ips_routers)
+    if SNMPRouters == None:
+        SNMPRouters = list()
+        for router in routers.keys():
+            SNMPRouters.append( SNMP.Snmp(nombreDevice=router , userName="R3SNMP",destHost=routers[router],protAuth="SHA",passAuth="RaDa22962") )
+
+    for SNMPServidor in  SNMPServidores:
+        info_S = SNMPServidor.getSNMP()
+        if info_S:
+            print(f"El {SNMPServidor.NombreDevice} : {SNMPServidor.DestHost} se encuentr activo ")
+        else:
+            print(f"El {SNMPServidor.NombreDevice} esta inactivo")
+    
+    for SNMPRouter in SNMPRouters:
+        info_R = SNMPRouter.getSNMP("sysName")
+        if info_R:
+            print(f"El {SNMPRouter.NombreDevice} : {SNMPRouter.DestHost} se encuentr activo ")
+        else:
+            print(f"El {SNMPRouter.NombreDevice} esta inactivo")
+
+def Opcion12():
+    with open("/home/warning.config","a+") as EmailFile:
+        EmailFile.seek(0,0)
+        email = EmailFile.read().strip()
+    if email == "":
+        emailNew = input("Ingrese el email al que se enviaran las alertas")
+    else:
+        print(f"El email al que se envian las advertencias actualmente es: {email}")
+        emailNew = input("Ingrese el email al que se enviaran las alertas")
+
+    res = re.search(r"[a-zA-Z0-9\._]+@[a-zA-Z\.]+\.[a-zA-Z]+",emailNew)
+    
+    if res != None:
+        print("Error la cadena ingresada no es un email")
+    else:
+        with open("/home/warning.config","w") as EmailFile:
+            EmailFile.write(emailNew)
+
+    print("Email modificado con exito")
 
 if __name__ == "__main__":
     os.popen(f"mkdir -p ~/Backups")
+
+    with open("/home/warning.config","a+") as EmailFile:
+        EmailFile.seek(0,0)
+        email = EmailFile.read().strip()
+    if email == "":
+        emailNew = input("Ingrese el email al que se enviaran las alertas")
+    else:
+        print(f"El email al que se envian las advertencias actualmente es: {email}")
+    
+    res = re.search(r"[a-zA-Z0-9\._]+@[a-zA-Z\.]+\.[a-zA-Z]+",emailNew)
+    
+    if res != None:
+        print("Error la cadena ingresada no es un email")
+    else:
+        with open("/home/warning.config","w") as EmailFile:
+            EmailFile.write(emailNew)
+
+    print("Email registrado con exito")
+
     while(True):
         os.system("clear")
         Menu()
@@ -239,7 +348,14 @@ if __name__ == "__main__":
             Opcion7()
         elif opcion == 8:
             Opcion8()
-
+        elif opcion == 9:
+            Opcion9()
+        elif opcion == 10:
+            Opcion10()
+        elif opcion == 11:
+            Opcion11()
+        elif opcion == 12:
+            Opcion12()    
         elif opcion == 13:
             break
 
@@ -249,70 +365,3 @@ if __name__ == "__main__":
                 break
         except:
             print("Opcion invalida")
-
-    # SNMP.prueba()
-    # BackupsFTP.prueba()
-
-# if __name__ == "__main__":
-#     
-#     ips_routers = obtener_ips_routers()
-#     routers = Obtener_ID_Router(ips_routers)
-    
-#     print("Los Router de la topologia son: \n")
-
-#     for router in routers.keys():
-#         print(f"Router: {router}, IP:{routers[router]}")
-    
-#     IDRouter = input("Ingrese el identificador del router que va a restaurar: ")
-#     if routers.get(IDRouter) :
-#         backs_F = list_backs_roruter(IDRouter)
-#         if backs_F: 
-#             print(f"Los backups del router {IDRouter} son:")
-#             print(backs_F)
-#             NameBack = input("Ingrese el nombre del backup:")
-#             if NameBack in backs_F:
-#                 SubirRespaldoRouter(routers[IDRouter],NameBack)
-#             else:
-#                 print("Archivo no encontrado")
-#         else:
-#             print(f"No Hay Backups del {IDRouter}")
-#     else:
-#         print(f"No existe el Router designado")
-    
-
-#     #print(routers)
-#     # for ip in ips_routers:
-#     #     obtener_backup(ip)
-#     # mover_backups()
-
-
-# def main():
-#     Servidor_1 = Snmp(nombreDevice="Servidor 1", userName="R3SNMP",destHost="192.168.226.18",protAuth="SHA-256",passAuth="RaDa22962")
-#     Servidor_2 = Snmp(nombreDevice="Servidor 2", userName="R3SNMP",destHost="192.168.226.26",protAuth="SHA-256",passAuth="RaDa22962")
-#     Router_1 = Snmp(nombreDevice="Router 1", userName="R3SNMP",destHost="192.168.226.49",protAuth="SHA",passAuth="RaDa22962")
-#     Router_2 = Snmp(nombreDevice="Router 2", userName="R3SNMP",destHost="192.168.226.50",protAuth="SHA",passAuth="RaDa22962")
-#     Router_3 = Snmp(nombreDevice="Router 3", userName="R3SNMP",destHost="192.168.226.54",protAuth="SHA",passAuth="RaDa22962")
-#     Router_4 = Snmp(nombreDevice="Router 4", userName="R3SNMP",destHost="192.168.226.58",protAuth="SHA",passAuth="RaDa22962")
-#     Router_5 = Snmp(nombreDevice="Router 5", userName="R3SNMP",destHost="192.168.226.33",protAuth="SHA",passAuth="RaDa22962")
-#     Router_6 = Snmp(nombreDevice="Router 6", userName="R3SNMP",destHost="192.168.226.42",protAuth="SHA",passAuth="RaDa22962")
-
-#     infoS1 = Servidor_1.getSNMP(DESCR_OID)
-#     infoS2 = Servidor_2.getSNMP(DESCR_OID)
-#     infoR1 = Router_1.getSNMP(DESCR_OID)
-#     infoR2 = Router_2.getSNMP(DESCR_OID)
-#     infoR3 = Router_3.getSNMP(DESCR_OID)
-#     infoR4 = Router_4.getSNMP(DESCR_OID)
-#     infoR5 = Router_5.getSNMP(DESCR_OID)
-#     infoR6 = Router_6.getSNMP(DESCR_OID)
-    
-#     print(f"La informacion del {Servidor_1.NombreDevice} es:\n {infoS1}")
-#     print(f"La informacion del {Servidor_2.NombreDevice} es:\n {infoS2}")
-#     print(f"La informacion del { Router_1.NombreDevice } es:\n {infoR1}")
-#     print(f"La informacion del { Router_2.NombreDevice } es:\n {infoR2}")
-#     print(f"La informacion del { Router_3.NombreDevice } es:\n {infoR3}")
-#     print(f"La informacion del { Router_4.NombreDevice } es:\n {infoR4}")
-#     print(f"La informacion del { Router_5.NombreDevice } es:\n {infoR5}")
-#     print(f"La informacion del { Router_6.NombreDevice } es:\n {infoR6}")
-
-# if __name__ == "__main__":
-#     main()
--- Scripts/SNMP.py
+++ Scripts/SNMP.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 import os
 
-INFO_ROUTERS_SNMP = None
-
 HOSTNAME_OID = "1.3.6.1.2.1.1.5.0"
 DESCR_OID = "1.3.6.1.2.1.1.1.0"
 CONTACT_OID = "1.3.6.1.2.1.1.4.0"
@@ -47,90 +45,131 @@ class Snmp(object):
             result = None
         return result
 
-def main():
-    
-    global INFO_ROUTERS_SNMP
 
-    Servidor_1 = Snmp(nombreDevice="Servidor 1", userName="R3SNMP",destHost="192.168.226.18",protAuth="SHA-256",passAuth="RaDa22962")
-    Servidor_2 = Snmp(nombreDevice="Servidor 2", userName="R3SNMP",destHost="192.168.226.26",protAuth="SHA-256",passAuth="RaDa22962")
-    Router_1 = Snmp(nombreDevice="Router 1", userName="R3SNMP",destHost="192.168.226.49",protAuth="SHA",passAuth="RaDa22962")
-    Router_2 = Snmp(nombreDevice="Router 2", userName="R3SNMP",destHost="192.168.226.50",protAuth="SHA",passAuth="RaDa22962")
-    Router_3 = Snmp(nombreDevice="Router 3", userName="R3SNMP",destHost="192.168.226.54",protAuth="SHA",passAuth="RaDa22962")
-    Router_4 = Snmp(nombreDevice="Router 4", userName="R3SNMP",destHost="192.168.226.58",protAuth="SHA",passAuth="RaDa22962")
-    Router_5 = Snmp(nombreDevice="Router 5", userName="R3SNMP",destHost="192.168.226.33",protAuth="SHA",passAuth="RaDa22962")
-    Router_6 = Snmp(nombreDevice="Router 6", userName="R3SNMP",destHost="192.168.226.42",protAuth="SHA",passAuth="RaDa22962")
+def InfoIntRouter(SNMPRouter = None):
 
-    
-    infoR1_ifDescr = Router_4.walkSNMP("ifDescr")
-    infoR1_ifAdminStatus = Router_4.walkSNMP("ifAdminStatus")
-    infoR1_ipAdEntNetMask = Router_4.walkSNMP("ipAdEntNetMask")
-    infoR1_ifName = Router_4.walkSNMP("ifName")
-    infoR1_ipAdEntIfIndex = Router_4.walkSNMP("ipAdEntIfIndex")
+    if SNMPRouter == None:
+        return None
 
-    limpiar = lambda info: [ linea.strip().split(" ")[3]  for linea in (info.strip().split("\n")) ]
-
-    infoR1_ipAdEntIfIndex = infoR1_ipAdEntIfIndex.strip().split("\n")
-    infoR1_ipAdEntNetMask = infoR1_ipAdEntNetMask.strip().split("\n")
-    
     int_IP_Num = {}
     IP_MASK = {}
 
-    for mask in infoR1_ipAdEntNetMask:
+    limpiar = lambda info: [ linea.strip().split(" ")[3]  for linea in (info.strip().split("\n")) ]
+
+    info_ifDescr        = SNMPRouter.walkSNMP("ifDescr")
+    info_ifAdminStatus  = SNMPRouter.walkSNMP("ifAdminStatus")
+    info_ipAdEntNetMask = SNMPRouter.walkSNMP("ipAdEntNetMask")
+    info_ifName         = SNMPRouter.walkSNMP("ifName")
+    info_ipAdEntIfIndex = SNMPRouter.walkSNMP("ipAdEntIfIndex")
+
+    if not( info_ifDescr and info_ifAdminStatus and info_ipAdEntNetMask and info_ifName and info_ipAdEntIfIndex ):
+        print(f"Error Obteniendo informacion del Router {SNMPRouter.NombreDevice}")
+        return False
+
+    info_ipAdEntIfIndex = info_ipAdEntIfIndex.strip().split("\n")
+    info_ipAdEntNetMask = info_ipAdEntNetMask.strip().split("\n")
+    
+    interfaces  = limpiar(info_ifDescr)        
+    estado      = limpiar(info_ifAdminStatus)  
+    contraccion = limpiar(info_ifName)
+
+    for mask in info_ipAdEntNetMask:
         mascara = mask.split(" ")[3]
-        ip = mask.split(" ")[0].split(".")
-        ip = ".".join(ip[1:])
-        
+        ip = mask.split(" ")[0].split(".")[1:]
+        ip = ".".join(ip)
         IP_MASK[ip] = mascara
-
-    for linea in infoR1_ipAdEntIfIndex:
+    
+    for linea in info_ipAdEntIfIndex:
         indice = int(linea.split(" ")[3])
-        ip = linea.split(" ")[0].split(".")
-        ip = ".".join(ip[1:])
-
+        ip = linea.split(" ")[0].split(".")[1:]
+        ip = ".".join(ip)
         int_IP_Num[indice] = list()
         int_IP_Num[indice].append(ip)
-
         if IP_MASK.get(ip) :
             int_IP_Num[indice].append( IP_MASK[ip] )
-
-
-    interfaces = limpiar(infoR1_ifDescr)        
-    estado = limpiar(infoR1_ifAdminStatus)  
-    contraccion = limpiar(infoR1_ifName)
-
-    INFO_ROUTERS_SNMP = dict()
-
+    
+    INFO_ROUTER_SNMP = list()
     if interfaces:
-        INFO_ROUTERS_SNMP[Router_4.NombreDevice] = list()
-        for numInt in len(interfaces):
-            INFO_ROUTERS_SNMP[Router_4.NombreDevice].append(list()) 
-            INFO_ROUTERS_SNMP[Router_4.NombreDevice][numInt].append( interfaces[numInt]     )
-            INFO_ROUTERS_SNMP[Router_4.NombreDevice][numInt].append( contraccion[numInt]    )
-            INFO_ROUTERS_SNMP[Router_4.NombreDevice][numInt].append( estado[numInt]         )
+        for numInt in range(len(interfaces)):
+            INFO_ROUTER_SNMP.append(list()) 
+            INFO_ROUTER_SNMP[numInt].append( interfaces[numInt]     )
+            INFO_ROUTER_SNMP[numInt].append( contraccion[numInt]    )
+            INFO_ROUTER_SNMP[numInt].append( estado[numInt]         )
             if int_IP_Num.get(numInt+1):
-                INFO_ROUTERS_SNMP[Router_4.NombreDevice][numInt].extend( int_IP_Num[numInt] )
+                INFO_ROUTER_SNMP[numInt].extend( int_IP_Num[numInt+1] )
     else:
         print("Error al obtener informacion de las interfaces")
+        return False
+    
+    print(f"Router: {SNMPRouter.NombreDevice}")
+    for interface in INFO_ROUTER_SNMP:
+        cadena = f"          {interface[0]:16}   {interface[1]:6}   {interface[2]:7}"
+        if len(interface) == 5:
+            cadena += f"   {interface[3]:15} {interface[4]:16}"
+        print(cadena)
 
-    print(int_IP_Num)
-    print(INFO_ROUTERS_SNMP)
-    #print(f"La informacion ifDescr del { Router_1.NombreDevice } es:\n {infoR1_ifDescr}")
-    #print(f"La informacion ifAdminStatus del { Router_1.NombreDevice } es:\n {infoR1_ifAdminStatus}")
-    #print(f"La informacion ipAdEntNetMask del { Router_1.NombreDevice } es:\n {infoR1_ipAdEntNetMask}")
-    #print(f"La informacion ifName del { Router_1.NombreDevice } es:\n {infoR1_ifName}")
-    #print(f"La informacion ipAdEntIfIndex del { Router_1.NombreDevice } es:\n {infoR1_ipAdEntIfIndex}")
-
-    #print(f"La informacion del {Servidor_1.NombreDevice} es:\n {infoS1}")
-    #print(f"La informacion del {Servidor_2.NombreDevice} es:\n {infoS2}")
-    #print(f"La informacion del { Router_1.NombreDevice } es:\n {infoR1}")
-    #print(f"La informacion del { Router_2.NombreDevice } es:\n {infoR2}")
-    #print(f"La informacion del { Router_3.NombreDevice } es:\n {infoR3}")
-    #print(f"La informacion del { Router_4.NombreDevice } es:\n {infoR4}")
-    #print(f"La informacion del { Router_5.NombreDevice } es:\n {infoR5}")
-    #print(f"La informacion del { Router_6.NombreDevice } es:\n {infoR6}")
-                                                                                                                                                                                                                                            
+    return True
 
 
+def InfoActividadRouter(SNMPRouter = None):
 
-if __name__ == "__main__":
-    main()
+    if SNMPRouter == None:
+        return None
+
+    int_IP_Num = {}
+    IP_MASK = {}
+
+    limpiar = lambda info: [ linea.strip().split(" ")[3]  for linea in (info.strip().split("\n")) ]
+
+    info_ifDescr        = SNMPRouter.walkSNMP("ifDescr")
+    info_ifAdminStatus  = SNMPRouter.walkSNMP("ifAdminStatus")
+    info_ipAdEntNetMask = SNMPRouter.walkSNMP("ipAdEntNetMask")
+    info_ipAdEntIfIndex = SNMPRouter.walkSNMP("ipAdEntIfIndex")
+
+    if not( info_ifDescr and info_ifAdminStatus and info_ipAdEntNetMask and info_ipAdEntIfIndex ):
+        print(f"Error Obteniendo informacion del Router {SNMPRouter.NombreDevice}")
+        return False
+
+    info_ipAdEntIfIndex = info_ipAdEntIfIndex.strip().split("\n")
+    info_ipAdEntNetMask = info_ipAdEntNetMask.strip().split("\n")
+    
+    interfaces  = limpiar(info_ifDescr)        
+    estado      = limpiar(info_ifAdminStatus)  
+
+    for mask in info_ipAdEntNetMask:
+        mascara = mask.split(" ")[3]
+        ip = mask.split(" ")[0].split(".")[1:]
+        ip = ".".join(ip)
+        IP_MASK[ip] = mascara
+    
+    for linea in info_ipAdEntIfIndex:
+        indice = int(linea.split(" ")[3])
+        ip = linea.split(" ")[0].split(".")[1:]
+        ip = ".".join(ip)
+        int_IP_Num[indice] = list()
+        int_IP_Num[indice].append(ip)
+        if IP_MASK.get(ip) :
+            int_IP_Num[indice].append( IP_MASK[ip] )
+    
+    INFO_ROUTER_SNMP = list()
+    if interfaces:
+        for numInt in range(len(interfaces)):
+            INFO_ROUTER_SNMP.append(list()) 
+            INFO_ROUTER_SNMP[numInt].append( interfaces[numInt]     )
+            INFO_ROUTER_SNMP[numInt].append( estado[numInt]         )
+            if int_IP_Num.get(numInt+1):
+                INFO_ROUTER_SNMP[numInt].extend( int_IP_Num[numInt+1] )
+    else:
+        print("Error al obtener informacion de las interfaces")
+        return False
+    
+    print(f"Router: {SNMPRouter.NombreDevice}")
+    for interface in INFO_ROUTER_SNMP:
+        if "up" in interfaces[1]:    
+            if len(interface) == 4:
+                cadena = f"          {interface[0]:16}   {interface[1]:7}   {interface[2]:15} {interface[3]:16}"
+            if len(interface) == 2:
+                cadena = f"          {interface[0]:16}   {interface[1]:7}"
+            print(cadena)
+
+    return True
